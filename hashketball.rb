1ace3b77f9b5108acbb907f3ec01912d4f3a1459
Amy Franz <amy.franz@gmail.com>
2020-01-10 23:50:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,6 +198,22 @@ def big_shoe_rebounds
   return rebound
 end
 
+def big_shoe_rebounds
+  shoe_size = 0
+  rebound = 0
+  game_hash.map do |place, team|
+    i = 0
+    while i < team[:players].length do
+      if team[:players][i][:shoe] > shoe_size
+        shoe_size = team[:players][i][:shoe]
+        rebound = team[:players][i][:rebounds]
+      end
+      i += 1
+    end
+  end
+  return rebound
+end
+
 
 
 
