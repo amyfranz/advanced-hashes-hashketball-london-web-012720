2e58a07cf3d3d0bc5a8d322c22eaa20987586135
Amy Franz <amy.franz@gmail.com>
2020-01-11 00:11:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -242,6 +242,7 @@ def player_with_longest_name
     while i < team[:players].length do
       puts team[:players][i][:player_name].length
       if team[:players][i][:player_name].length > length_name
+        puts team[:players][i][:player_name].length
         length = team[:players][i][:player_name].length
         name = team[:players][i][:player_name]
       end
